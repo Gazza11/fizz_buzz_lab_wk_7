2c3f9248288c1458f4b8c2ed08c21b46d50ee871
Gary Darnell <gdarnell9@gmail.com>
2021-06-22 11:04:33 +0100

    New way of function.

--- src/App.js
+++ src/App.js
@@ -10,23 +10,8 @@ const handleInc = () => {
   setNumber(inputNumber + 1)
 }
 
-// const fizzBuzzLogic = (inputNumber) => {
-//   if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
-//     setAnswer("fizzbuzz")
-//   } else if (inputNumber % 3 === 0) {
-//     setAnswer("fizz")
-//   } else if (inputNumber % 5 === 0) {
-//     setAnswer("buzz")
-//   } else {
-//     setAnswer(inputNumber);
-//   }
-// }
-
-// useEffect(fizzBuzzLogic(number), [number])
-
-useEffect(() => {
-  console.log(inputNumber)
-    if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+const fizzBuzzLogic = () => {
+  if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
     setAnswer("fizzbuzz")
   } else if (inputNumber % 3 === 0) {
     setAnswer("fizz")
@@ -34,8 +19,23 @@ useEffect(() => {
     setAnswer("buzz")
   } else {
     setAnswer(inputNumber);
-  } console.log(answer)
-}, [inputNumber])
+  }
+}
+
+useEffect(fizzBuzzLogic, [inputNumber])
+
+// useEffect(() => {
+//   console.log(inputNumber)
+//     if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+//     setAnswer("fizzbuzz")
+//   } else if (inputNumber % 3 === 0) {
+//     setAnswer("fizz")
+//   } else if (inputNumber % 5 === 0) {
+//     setAnswer("buzz")
+//   } else {
+//     setAnswer(inputNumber);
+//   } console.log(answer)
+// }, [inputNumber])
 
   return (
     <div>
